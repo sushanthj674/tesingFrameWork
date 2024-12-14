616d801052b234b09adaeb57a42362aaac15ff8a
sushanth <153906968+sushanthj674@users.noreply.github.com>
2024-12-15 01:38:46 +0530

    Create frameWork.js

---
+++ frameWork.js
@@ -0,0 +1,61 @@
+const add = function (firstNumber, secondNumber) {
+  return firstNumber + secondNumber;
+};
+
+const concatStrings = function (...strings) {
+  return ''.concat(...strings);
+};
+
+const concatArrays = function (...arrays) {
+  return [].concat(...arrays);
+};
+
+// frame work 
+
+const isTestFailed = function (test) {
+  const [expected, actual] = [test[2], test[3]];
+
+  const isEqual = expected === actual && !Array.isArray(expected);
+  let isArrayEqual = Array.isArray(expected);
+  isArrayEqual = isArrayEqual && expected.join() === actual.join();
+
+  return !(isEqual || isArrayEqual);
+};
+
+const execute = function (params) {
+  const [functionName, parameters, expected] = params;
+  const actual = functionName(...parameters);
+
+  return Array(functionName, parameters, expected, actual);
+};
+
+const test = function (allTests) {
+  const excutedTests = allTests.map(execute);
+
+  return excutedTests.filter(isTestFailed);
+};
+
+const testAll = function (allTests) {
+  const failedTests = test(allTests);
+  const isAllTestsPassed = failedTests.length === 0;
+  const testsPassed = allTests.length - failedTests.length;
+
+  if (!isAllTestsPassed) {
+    console.table(failedTests);
+  }
+
+  console.log(isAllTestsPassed ? 'all' : testsPassed, ' tests Passed!!');
+};
+
+const allTests = [
+  [add, [1, 2], 3],
+  [add, [1, 2], 4],
+  [concatStrings, ['hello', 'world'], 'helloworld'],
+  [concatStrings, ['hello', 'world'], 'helloworl'],
+  [concatArrays, [['hello'], ['world']], ['hello', 'world']],
+  [concatArrays, [['hello'], ['world']], ['hello', 'worl']],
+  [concatArrays, [[1], [2]], [1, 2]],
+  [concatArrays, [[1], [2]], [1, 1]]
+];
+
+testAll(allTests);
